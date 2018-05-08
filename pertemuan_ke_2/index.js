2efae66b9628386e105d5858b9a507ba0afd4679
Rifky <notlog11c>
2018-05-08 20:27:02 +0700

    types of writing function

--- pertemuan_ke_2/index.js
+++ pertemuan_ke_2/index.js
@@ -1,6 +1,38 @@
-function name(...params) 
-{
-    console.log(typeof params)
-}
+/**
+ * cara ke 1
+ * 1 Function biasa
+ */
 
-name(1,2,3,4,5,6,7,8,9)
+// function isEven(number) 
+// {
+//     return number % 2 === 0
+// }
+
+/**
+ * cara ke 2
+ * 2. Function simpan di variable
+ */
+
+// const isEven = function(number) {
+//     return number % 2 === 0
+// }
+
+/** 
+ * cara ke 3
+ * 3. arrow Function
+ */
+
+// const isEven = (number) => {
+//     return number % 2 === 0
+// }
+
+/**
+ * cara ke 3 bs seperti ini
+ * tapi, jika parameter(number adalah parameternya) lebih dari satu, maka menggunakan ()
+ * ini juga digunakan jika data hanya return 1 data. 
+ */
+
+const isEven = number => number % 2 === 0
+
+console.log(isEven(10), 10)
+console.log(isEven(7), 7)
